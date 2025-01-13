8f6719004be92a813aa86b3d0722bc43dbad2fda
Anshul Goel <anshul.go87@gmail.com>
2025-01-13 20:05:33 +0530

    set kickoff args

--- src/meme_generator_flow/main.py
+++ src/meme_generator_flow/main.py
@@ -14,18 +14,18 @@ class MemeState(BaseModel):
 
 class MemeFlow(Flow[MemeState]):
 
-    @start()
-    def gather_meme_topic(self):
-        print("Gathering user input")
-        self.state.meme_topic = input("Enter the topic of meme: ")
+    # @start()
+    # def gather_meme_topic(self):
+    #     print("Gathering user input")
+    #     self.state.meme_topic = input("Enter the topic of meme: ")
 
-    @listen(gather_meme_topic)
+    @start()
     def generate_meme_idea(self):
         print("Generating meme idea")
         result = (
             MemeIdeaCrew()
             .crew()
-            .kickoff(inputs={"meme_topic": self.state.meme_topic})
+           .kickoff(inputs={"meme_topic": self.state.meme_topic})
         )
 
         print("Idea generated", result.raw)
@@ -50,9 +50,10 @@ class MemeFlow(Flow[MemeState]):
     #         f.write(self.state.meme_idea)
 
 
-def kickoff():
+def kickoff(meme_topic: str):
+    inputs = {"meme_topic": meme_topic}
     meme_idea_flow = MemeFlow()
-    meme_idea_flow.kickoff()
+    meme_idea_flow.kickoff(inputs=inputs)
 
 
 def plot():
